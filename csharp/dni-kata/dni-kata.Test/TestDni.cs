4f935e9b4435c27d1a87a4dc4c070ece4b9c8c1d
Luis Rovirosa Mairlot <luis@codium.team>
2022-07-01 18:30:56 +0200

    refactor: remove unneeded test

--- csharp/dni-kata/dni-kata.Test/TestDni.cs
+++ csharp/dni-kata/dni-kata.Test/TestDni.cs
@@ -3,12 +3,6 @@ namespace dni_kata.Test;
 public class TestDni
 {
     [Fact]
-    public void RenameMe()
-    {
-        Assert.True(true);
-    }
-
-    [Fact]
     public void Test_Dni_ShouldBeNotLongerThan8()
     {
         Assert.Throws<Exception>(() => new Dni("123456789"));
