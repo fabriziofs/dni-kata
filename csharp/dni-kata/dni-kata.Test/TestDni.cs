d3b1a27ba6abbcb4228acd222e3e1d40180f49b8
Giacomo Policicchio <pgiacomo69@yahoo.com>
2022-07-01 18:29:27 +0200

    Primer Test

--- csharp/dni-kata/dni-kata.Test/TestDni.cs
+++ csharp/dni-kata/dni-kata.Test/TestDni.cs
@@ -7,4 +7,18 @@ public class TestDni
     {
         Assert.True(true);
     }
+
+    [Fact]
+    public void Test_Dni_ShouldBeNotLongerThan8()
+    {
+        Assert.Throws<Exception>(() => new Dni("123456789"));
+    }
+}
+
+public class Dni
+{
+    public Dni(string s)
+    {
+        throw new Exception();
+    }
 }
