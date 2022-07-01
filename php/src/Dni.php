2aaccc4668ab6a69b27a34a78936a98c8b88a954
Chemaclass <chemaclass@outlook.es>
2022-07-01 19:33:11 +0200

    Refacoring: extract method in fromString()

--- php/src/Dni.php
+++ php/src/Dni.php
@@ -12,13 +12,8 @@ class Dni
 
     public static function fromString(string $value): self
     {
-        if (strlen($value) !== self::VALID_LENGTH) {
-            throw new RuntimeException('invalid length');
-        }
-
-        if (preg_match('/\d$/', $value[self::VALID_LENGTH - 1])) {
-            throw new RuntimeException('the last char must be a letter');
-        }
+        self::checkLength($value);
+        self::checkLastCharMustBeALetter($value);
 
         if (!preg_match('/\d+$/', substr($value, 0, -1))) {
             throw new RuntimeException('all chars but last one must be numeric');
@@ -33,6 +28,20 @@ class Dni
 
     public function __construct(private string $value)
     {
+    }
+
+    private static function checkLength(string $value): void
+    {
+        if (strlen($value) !== self::VALID_LENGTH) {
+            throw new RuntimeException('invalid length');
+        }
+    }
+
+    private static function checkLastCharMustBeALetter(string $value): void
+    {
+        if (preg_match('/\d$/', $value[self::VALID_LENGTH - 1])) {
+            throw new RuntimeException('the last char must be a letter');
+        }
     }
 
     public function toString(): string
