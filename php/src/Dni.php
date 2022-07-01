1b62a162b58a6e5caff674f5cb30ca4a30cbbdc1
JesusValera <jesus1902@outlook.com>
2022-07-01 19:30:34 +0200

    Refactor: Extract logic into methods

--- php/src/Dni.php
+++ php/src/Dni.php
@@ -15,18 +15,31 @@ final class Dni
 
     public static function create(string $dni): self
     {
+        self::thrownExceptionIfInvalidLength($dni);
+        self::thrownExceptionIfInvalidFormat($dni);
+        self::thrownExceptionIfInvalidCharacter($dni);
+
+        return new self($dni);
+    }
+
+    private static function thrownExceptionIfInvalidLength(string $dni): void
+    {
         if (mb_strlen($dni) !== self::DNI_LENGTH) {
             throw DniException::invalidLength();
         }
+    }
 
+    private static function thrownExceptionIfInvalidFormat(string $dni): void
+    {
         if (preg_match('/\d{8}[A-ZÑ]/u', $dni) !== 1) {
             throw DniException::invalidFormat();
         }
+    }
 
+    private static function thrownExceptionIfInvalidCharacter(string $dni): void
+    {
         if (in_array(mb_substr($dni, -1), self::INVALID_CHARACTERS)) {
             throw DniException::invalidLetter();
         }
-
-        return new self($dni);
     }
 }
