e550264e994b9dae5b200334f74097227cb9eeb3
Fabrizio Fasanando <62360034+fabriziofs@users.noreply.github.com>
2022-07-02 18:22:51 +0200

    [🟢] Check if dni string is nine characters long

--- php/src/Dni.php
+++ php/src/Dni.php
@@ -2,10 +2,14 @@
 
 namespace Kata;
 
+use RuntimeException;
+
 class Dni
 {
     public function __construct(string $dni)
     {
-
+        if (mb_strlen($dni) !== 9) {
+            throw new RuntimeException();
+        }
     }
 }
