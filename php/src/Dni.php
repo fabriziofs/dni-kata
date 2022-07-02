78d3a14df833426a77952f71185f2ae60d473830
Fabrizio Fasanando <62360034+fabriziofs@users.noreply.github.com>
2022-07-02 19:12:50 +0200

    [🔵] Extract validations to methods

--- php/src/Dni.php
+++ php/src/Dni.php
@@ -6,15 +6,27 @@ class Dni
 {
     public function __construct(string $dni)
     {
+        $this->ensureValidLength($dni);
+        $this->ensureValidFormat($dni);
+        $this->ensureValidLetter($dni);
+    }
+
+    private function ensureValidLength(string $dni): void
+    {
         if (mb_strlen($dni) !== 9) {
             throw new DniInvalidLengthException();
         }
+    }
 
-        if (!preg_match('/\d{8}[A-ZÑ]/u', $dni))
-        {
+    private function ensureValidFormat(string $dni): void
+    {
+        if (!preg_match('/\d{8}[A-ZÑ]/u', $dni)) {
             throw new DniInvalidFormatException();
         }
+    }
 
+    private function ensureValidLetter(string $dni): void
+    {
         if (in_array(mb_substr($dni, -1), ['I', 'Ñ', 'O', 'U'])) {
             throw new DniInvalidLetterException();
         }
