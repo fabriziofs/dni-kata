cc36c81a0c413bc0f2564262d404c0f31eb35e6a
Fabrizio Fasanando <62360034+fabriziofs@users.noreply.github.com>
2022-07-02 19:42:51 +0200

    [🟢] Check if the number match with the letter

--- php/src/Dni.php
+++ php/src/Dni.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Kata;
 
@@ -6,11 +8,38 @@ class Dni
 {
     private const VALID_LENGTH = 9;
 
+    private const LETTER_MAPPER = [
+        0  => 'T',
+        1  => 'R',
+        2  => 'W',
+        3  => 'A',
+        4  => 'G',
+        5  => 'M',
+        6  => 'Y',
+        7  => 'F',
+        8  => 'P',
+        9  => 'D',
+        10 => 'X',
+        11 => 'B',
+        12 => 'N',
+        13 => 'J',
+        14 => 'Z',
+        15 => 'S',
+        16 => 'Q',
+        17 => 'V',
+        18 => 'H',
+        19 => 'L',
+        20 => 'C',
+        21 => 'K',
+        22 => 'E',
+    ];
+
     public function __construct(string $dni)
     {
         $this->ensureValidLength($dni);
         $this->ensureValidFormat($dni);
         $this->ensureValidLetter($dni);
+        $this->ensureValidLetterForThatNumbers($dni);
     }
 
     private function ensureValidLength(string $dni): void
@@ -30,6 +59,20 @@ class Dni
     private function ensureValidLetter(string $dni): void
     {
         if (in_array(mb_substr($dni, -1), ['I', 'Ñ', 'O', 'U'])) {
+            throw new DniInvalidLetterException();
+        }
+    }
+
+    private function ensureValidLetterForThatNumbers(string $dni): void
+    {
+        $number = (int)substr($dni, 0, 8);
+
+        $result = $number % 23;
+
+        $correctLetter = self::LETTER_MAPPER[$result];
+        $actualLetter = mb_substr($dni, -1);
+
+        if ($correctLetter !== $actualLetter) {
             throw new DniInvalidLetterException();
         }
     }
